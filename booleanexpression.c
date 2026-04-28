a51364bee33087307000521f8d71b3e53911144d
code07ranger <harshmahawar47@gmail.com>
2026-04-29 00:27:54 +0530

    Add files via upload

---
+++ booleanexpression.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+   int x=3,y,z;
+   y=x=10;
+   z=x<10; //  boolean comment whether relation if true ->1 ,,false->0
+   printf("x=%d,y=%d,z=%d",x,y,z);
+
+    return 0;
+
+} 
